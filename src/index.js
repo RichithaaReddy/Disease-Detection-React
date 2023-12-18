8cec4d0c2930ecf5bc2f2a5c38191fbe26bdae27
RICHITHA REDDY <104646601+RichithaaReddy@users.noreply.github.com>
2023-12-18 20:12:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CloudinaryContextProvider } from './CloudinaryConfig';
+// import { CloudinaryContextProvider } from './CloudinaryConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
